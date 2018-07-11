290d22976e4b9c6514c6b6fcd70707ce8456e34d
abdarum <kornel@KornelLaptop.MyHomeServer.com>
2018-07-11 22:03:39 +0200

    new project comparator of parts

---
+++ part_list_comparator.py
@@ -0,0 +1,20 @@
+#python2.7
+
+class DataIndex:
+    def __init__(self):
+        #index kolumny
+        #index w kontenerze
+        pass
+
+
+class DataConfiguration:
+    def __init__(self):
+        #okreslenie ktora kolumna za co odpowiada
+        pass
+
+
+class Container:
+    def __init__(self):
+        #ogolna klasa dzialjaca bez konkretnych nazw
+        pass
+
