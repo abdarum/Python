94da2b7bbd4ef4a7d5808e3122b814a766dc18d4
Praktikant 3 WRO <praktikant3_wro@wrolsv002.wro.schu>
2018-07-31 07:59:59 +0200

    printing all diffs

--- part_list_comparator.py
+++ part_list_comparator.py
@@ -360,19 +360,37 @@ class Container:
             if len(i)>1:
                 for idx in range(0,self.cont_conf.max_c_idx()):
                     if self.cont_conf.must_be_equal(idx):
+                        tmp_diff_list = [ [],
+                            idx,
+                            self.cont_conf.name_c_idx(idx),
+                            self.cont_conf.descr_c_idx(idx)]
                         self.tmp_sort_key = idx
                         i.sort(key=self.sort_tmp_key)
                         for grp_elem_idx in range(1,len(i)):
-                            if i[grp_elem_idx-1][idx]\
-                                    != i[grp_elem_idx][idx]:
-                                #modif if you need
-                                self.diff_group.append(
-                                    [
-                                    [i[grp_elem_idx-1],
-                                    i[grp_elem_idx]],
-                                    idx,
-                                    self.cont_conf.name_c_idx(idx),
-                                    self.cont_conf.descr_c_idx(idx)])
+                            for elem_comp_idx in range(grp_elem_idx+1,len(i)):
+                                if i[grp_elem_idx][idx]\
+                                       != i[elem_comp_idx][idx]:
+                                    if self.is_on_list(i[grp_elem_idx][idx],
+                                            tmp_diff_list[0],idx) is False:
+                                        tmp_diff_list[0].append(i[grp_elem_idx])
+                                    print(i[grp_elem_idx])
+                                    
+                        print(tmp_diff_list[0])
+                        if len(tmp_diff_list[0]):
+                            self.diff_group.append(tmp_diff_list)
+ 
+                        """
+                        if i[grp_elem_idx-1][idx]\
+                                != i[grp_elem_idx][idx]:
+                            #modif if you need
+                            self.diff_group.append(
+                                [
+                                [i[grp_elem_idx-1],
+                                i[grp_elem_idx]],
+                                idx,
+                                self.cont_conf.name_c_idx(idx),
+                                self.cont_conf.descr_c_idx(idx)])
+                        """
 
                                     
 
@@ -393,14 +411,13 @@ class Container:
         print("KTM code: "+str(i[0][0][self.cont_conf.\
             c_idx_of('ktm_code')]))
 
-        print('Page ',i[0][0][1],' Idx ',i[0][0][0],
-                'Schem idx',i[0][0][3], 'Part',i[0][0][9])
-        print('Page ',i[0][1][1],' Idx ',i[0][1][0],
-                'Schem idx',i[0][1][3], 'Part',i[0][1][9])
+        for j in i[0]:
+            print('Page ',j[1],' Idx ',j[0],
+                    'Schem idx',j[3], 'Part',j[9])
 
         print('Diffrence:')
-        print(i[0][0][i[1]])
-        print(i[0][1][i[1]])
+        for j in i[0]:
+            print(j[i[1]])
 
     def fix_all_differences(self):
         for i in range(0,len(self.diff_group)):
