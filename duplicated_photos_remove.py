aea2036a582739327632e831a548bdba538240ae
abdarum <kornelstefanczyk@wp.pl>
2020-08-24 21:50:51 +0200

    added duplicate scan

--- duplicated_photos_remove.py
+++ duplicated_photos_remove.py
@@ -1,6 +1,6 @@
 import os
-import sys
 import pprint
+import sys
 
 # source_path = "C:\\Kornel_Zdjecia\\tmp_script_test_source_Camera2"
 # destination_path = "C:\\Kornel_Zdjecia\\tmp_script_test_source_Gallery_backup"
@@ -17,32 +17,49 @@ class Duplicates:
         def __init__(self, list_of_paths, name):
             self.list_of_paths = list_of_paths
             self.name = name
-            pass
 
+        def get_name(self):
+            return self.name
+
+        def set_name(self, name):
+            self.name = name
+
+        def get_list_of_paths(self):
+            return self.list_of_paths
+
+        def set_list_of_paths(self, list_of_paths):
+            # 'list_of_paths' is list of paths containing duplicated files
+            self.list_of_paths = list_of_paths
+
+        def add_paths_to_list(self, list_of_paths):
+            # 'list_of_paths' is list of paths containing duplicated files
+            for path in list_of_paths: 
+                self.list_of_paths.append(path)
+
+        def print_duplicate_item(self):
+            print("Name: {}\nPaths".format(self.name))
+            pprint.pprint(self.list_of_paths)
+            print("")
 
     def __init__(self):
         self.list_of_duplicate_items = []
 
     def create_duplicate(self, paths):
         # 'paths' is list of paths containing duplicated files
-        #todo
-
-        # for path in paths:
-        #     for i in self.list_of_duplicate_items:
-        #         os.path.split(path)[1]
+        name = os.path.split(paths[0])[1]
+        list_of_paths = []
+        for path in paths:
+            list_of_paths.append(os.path.split(path)[0])
         
-        print('create duplicates')
-        pprint.pprint(paths)
-        print("")
-        pass
+        self.list_of_duplicate_items.append(self.DuplicateItem(list_of_paths, name))
 
     def add_to_existing_duplicate(self, paths):
         # 'paths' is list of paths containing duplicated files
         #todo
-        print('update duplicates')
-        pprint.pprint(paths)
-        print("")
-        pass
+        for path in paths:
+            for i in self.list_of_duplicate_items:
+                if i.name == os.path.split(path)[1]:
+                    i.add_paths_to_list([os.path.split(path)[0]])
 
     def duplicate_exist_in_base(self, paths):
         # 'paths' is list of paths containing duplicated files
@@ -52,6 +69,12 @@ class Duplicates:
                     return True
         return False
 
+    def print_duplicates(self):
+        print("\n\n***********************\n****   DUPLICATES  ****\n***********************")
+        for duplicate_item in self.list_of_duplicate_items:
+            duplicate_item.print_duplicate_item()
+            
+            
 class DirectoryStructure:
     def __init__(self, root_directory=None):
         self.found_not_processed_files = []
@@ -61,6 +84,8 @@ class DirectoryStructure:
         self.root_directory = root_directory
 
     def classify_file(self, full_file_path):
+        self.find_duplicates(full_file_path)
+        
         extension = os.path.splitext(full_file_path)[1]
         if extension in TRUSTED_EXTENSIONS:
             self.trusted_files.append(full_file_path)
@@ -68,8 +93,6 @@ class DirectoryStructure:
             self.ignored_files.append(full_file_path)
         else:
             self.found_not_processed_files.append(full_file_path)
-        
-        self.find_duplicates(full_file_path)
 
     def scan_directory(self, full_file_path=None):
         if full_file_path is None:
@@ -82,8 +105,8 @@ class DirectoryStructure:
             if self.duplicated_files.duplicate_exist_in_base([full_file_path]):
                 self.duplicated_files.add_to_existing_duplicate([full_file_path])
             else:
-                duplicates = [f for f in self.trusted_files \
-                        if (os.path.split(f)[1] == os.path.split(full_file_path)[1])]
+                duplicates = [f for f in self.trusted_files if (os.path.split(f)[1] == os.path.split(full_file_path)[1])]
+                duplicates.append(full_file_path)
                 self.duplicated_files.create_duplicate(duplicates)
 
     def print_warnings(self):
@@ -96,7 +119,11 @@ class DirectoryStructure:
         for f in self.ignored_files:
             print("IGNORED: File was ignored:\n{}\n".format(f))
 
+    def print_duplicates(self):
+        self.duplicated_files.print_duplicates()
+
 source = DirectoryStructure(source_path)
 source.scan_directory()
 source.print_warnings()
 source.print_ignored()
+# source.print_duplicates()
