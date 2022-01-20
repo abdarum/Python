43f6500432b80fe089ed28292b718ee7db2a3e1e
abdarum <kornelstefanczyk@wp.pl>
2022-01-21 00:23:38 +0100

    Parsing Stooq table

--- dividend_listing.py
+++ dividend_listing.py
@@ -18,10 +18,15 @@ bossa_file_path = r'C:\Users\Kornel\Desktop\tmp\Dywidendy\20220104_230700_Dywide
 #############################
 ######     Classes     ######
 #############################
+class ErrorDef:
+    PARSE_STOOQ_OPERATION_TABLE_NOT_DIVIDEND_IN_ROW = 'Error parsing, row in the Stooq operation table. Operation different than dividend'
+
 class DividendItem:
-    def __init__(self):
-        self.percent = None
-        self.nominal = None
+    def __init__(self, date=None, percent=None, nominal=None, rights_issue=None):
+        self.date = date
+        self.percent = percent
+        self.nominal = nominal
+        self.rights_issue = rights_issue
 
 class DividendCompanyItem:
     def __init__(self,
@@ -29,12 +34,18 @@ class DividendCompanyItem:
             name_short=None,
             name_long=None,
             sector_gpw=None,
-            sector_name=None):
+            sector_name=None,
+            dividends_list=None):
         self.bossa_instrument_url = bossa_instrument_url
         self.name_short = name_short
         self.name_long = name_long
         self.sector_gpw = sector_gpw
         self.sector_name = sector_name
+        self.dividends_list = []
+        if dividends_list != None:
+            assert isinstance(dividends_list, list)
+            self.dividends_list.extend(dividends_list)
+
 
     def __str__(self):
         return str(self.__dict__)
@@ -49,31 +60,6 @@ class DividendCompaniesContainer:
     BIZNESRADAR_INSTRUMENTS_URL = 'https://www.biznesradar.pl/operacje/' # next long name of instrument e.g. ATLANTAPL
     STOOQ_INSTRUMENTS_URL = 'https://stooq.pl/q/m/?s=' # next short name of instrument e.g. zwc
     HTTP_TIMEOUT_SECONDS = 5
-
-    headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
-        'Cookie': 'FCCDCF=[null,null,null,["CPS4aoPPS4aoPEsABBPLB-CoAP_AAH_AAB5YHQpD7T7FbSFCyP55fLsAMAhXRkCEAqQAAASABmABQAKQIAQCkkAQFASgBAACAAAgICZBAQAMCAgACUABQABAAAEEAAAABAAIIAAAgAEAAAAIAAACAIAAAAAIAAAAEAAAmwgAAIIACAAABAAAAAAAAAAAAAAAAgdCgPsLsVtIUJI_Gk8uwAgCFdGQIQCoAAAAIAGYAAAApAgBAKQQBAABKAAAAIAACAgJgEBAAgACAABQAFAAEAAAAAAAAAAAAggAACAAQAAAAgAAAIAgAAAAAgAAAAAAACBCAAAggAIAAAAAAAAAAAAAAAAAAACAAA.f-gAAAAAAAA","1~2072.66.70.89.93.108.122.149.2202.162.167.196.2253.241.2299.259.2357.311.317.323.2373.338.358.415.440.449.2506.2526.482.486.494.495.2568.2571.2575.540.574.2677.817.864.981.1051.1095.1097.1127.1201.1205.1211.1276.1301.1365.1415.1449.1451.1570.1577.1651.1716.1765.1870.1878.1889","E04CC7CA-B4E7-4DB1-B094-7988B95FF5E3"],null,null,[]]; FCNEC=[["AKsRol-ZW-cUg7_Jfj3sVnDUsNiZM4BhtDLM6iUdyRrgGOJ3WAU8xrY10EctRdI68cuuChzvhd2EENRxttugLMWUzh2PZdjHKEBiYVZ9-hj6FOxdzZK7d7LplfqUo-qj1YnG8P2GxR6eNOCe93kO6E8saaYVg3zqjw=="],null,[]]; privacy=1642290642; PHPSESSID=g33e2uc55nhuf6tkojiialhto2; uid=plbnvv7f37jaqv90e7lf0dcxx8; _ga=GA1.2.60653019.1642290643; cookie_uu=220119000; _gid=GA1.2.1761052878.1642605637; cookie_user=?0001dllg000011800d1300e3|zwc+aca+nvt+atp+sfg+sev+krc+kgh',
-        'DNT': '1',
-        'Host': 'stooq.pl',
-        # 'If-Modified-Since': 'Wed, 19 Jan 2022 16:59:51 GMT',
-        'Referer': 'https://stooq.pl/pomoc/?q=9&s=zwc',
-        'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform:': '"Windows"',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'same-origin',
-        'Sec-Fetch-User': '?1',
-        'Upgrade-Insecure-Requests': '1',
-        'xxx': 'xxx',
-        'xxx': 'xxx',
-        'xxx': 'xxx',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
-    }
 
     #############################
     ######  Class methods  ######
@@ -114,17 +100,87 @@ class DividendCompaniesContainer:
             btn.click()
             time.sleep(self.HTTP_TIMEOUT_SECONDS)
 
+    def parse_stooq_operation_table(self, table_html=None):
+        err_method = None
+        html_str = table_html
 
-    def test_web_browser(self, url=None):
-        print('test')
+        table = BeautifulSoup(html_str, 'html.parser')
+        columns_names = {}
+
+        dividends_list = []        
+        for row_idx, row in enumerate(table.find_all('tr')):
+            err_row = None
+            columns = row.find_all('td')
+            if len(columns) == 0:
+                columns = row.find_all('th')
+                columns = [elem.text for elem in columns]
+                for col_idx, col_val in enumerate(columns):
+                    columns_names[col_idx] = col_val
+            else:
+                columns = [elem.text for elem in columns]
+                row_dict = {}
+
+                for col_idx, col_val in enumerate(columns):
+                    row_dict[columns_names[col_idx]] = col_val
+                
+                # parse values - adjust to the schema
+                for key in row_dict.keys():
+                    if key == 'Data':
+                        row_dict[key] = row_dict.get(key)
+                    elif key == 'Zdarzenie':
+                        if row_dict.get(key).startswith('Dywidenda '):
+                            val_str = row_dict.get(key).replace('Dywidenda ', '').replace('%', '')
+                            try:
+                                row_dict[key] = float(val_str)
+                            except:
+                                pass
+                        else:
+                            err_row = ErrorDef.PARSE_STOOQ_OPERATION_TABLE_NOT_DIVIDEND_IN_ROW
+
+                    elif key == 'Nominalnie':
+                        try:
+                            row_dict[key] = float(row_dict.get(key))
+                        except:
+                            pass
+                        
+                    elif key == 'Dzielnik':
+                        try:
+                            row_dict[key] = float(row_dict.get(key))
+                        except:
+                            pass
+
+                dividend_item = DividendItem(
+                    date=row_dict.get('Data'), 
+                    percent=row_dict.get('Zdarzenie'),
+                    nominal=row_dict.get('Nominalnie'),
+                    rights_issue=row_dict.get('Dzielnik')
+                    )
+                
+                if err_row:
+                    print('Parsing error, dividend_item skipped. Dividend item: {}. Error {}'.format(dividend_item, err_row))
+                else:
+                    dividends_list.append(dividend_item)
+
+        return dividends_list
+
+
+    def parse_stooq_operations_website(self, short_name=None):
+        url = self.STOOQ_INSTRUMENTS_URL + short_name
         self.driver.get(url)
         time.sleep(self.HTTP_TIMEOUT_SECONDS)
         self.__webdriver_agree_cookies_stood()
 
         # fth1
-        id_box = self.driver.find_element_by_id('fth1')
+        table_item = self.driver.find_element_by_id('fth1')
+        # table
 
-        print('test')
+        table_html = table_item.get_attribute('innerHTML')
+        dividends_list = self.parse_stooq_operation_table(table_html)
+
+        ret_dict = {}
+        ret_dict['dividends_list'] = dividends_list
+
+        return ret_dict
 
     def parse_bossa_dividend_html(self, bossa_file_path=None, bossa_url=None):
         assert bossa_file_path or bossa_url, 'Source wasn\'t set correctly'
@@ -140,15 +196,17 @@ class DividendCompaniesContainer:
             if( not link_url is None and link_url.startswith(self.BOSSA_INSTRUMENTS_URL) ):
                 instrument_short = link_url.replace(self.BOSSA_INSTRUMENTS_URL, '')
                 instrument_long = link.text
+                stooq_content = self.parse_stooq_operations_website(instrument_short)
+                dividends_list = stooq_content.get('dividends_list')
 
                 dividend_company_item = DividendCompanyItem(
                     bossa_instrument_url = link_url,
                     name_short = instrument_short,
                     name_long = instrument_long)
 
-                soup = self.test_web_browser(self.STOOQ_INSTRUMENTS_URL+instrument_short)
 
                 self.append(dividend_company_item)
+                print('test')
 
 #############################
 ###### BUSINESS LOGIC  ######
