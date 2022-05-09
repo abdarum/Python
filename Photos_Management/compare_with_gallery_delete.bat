a28fdf2996450145cb4373131dd157113aab392d
abdarum <kornelstefanczyk@wp.pl>
2022-05-09 16:31:33 +0200

    Run duplicate script template

---
+++ Photos_Management/compare_with_gallery_delete.bat
@@ -0,0 +1,3 @@
+@REM python C:\GitHub\Python\duplicated_photos_remove.py -v -p -r
+@REM python C:\GitHub\Python\duplicated_photos_remove.py -P C:\\GitHub\\Python\\Photos_Management\\config_file.json 
+pause
